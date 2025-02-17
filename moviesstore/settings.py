a8ae529f4f9d2d122dbe6be74edb33a954abf116
advenumud <159509217+advenumud@users.noreply.github.com>
2025-02-17 12:19:47 -0500

    cart and reviews

--- moviesstore/settings.py
+++ moviesstore/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'home',
     'movies',
     'accounts',
+    'cart',
 ]
 
 MIDDLEWARE = [
@@ -110,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'EST'
 
 USE_I18N = True
 
